f31289b08bc76856dfdf8905f5c2b4baec30e763
zhangzhenwei <zhenwei1108@163.com>
2021-12-01 22:45:13 +0800

    增加 channel-> transferFrom实现拷贝

--- network/src/main/java/com/github/zhenwei/network/io/nio/basic/BasicChannel.java
+++ network/src/main/java/com/github/zhenwei/network/io/nio/basic/BasicChannel.java
@@ -15,7 +15,7 @@ import java.nio.channels.WritableByteChannel;
  */
 public class BasicChannel {
 
-  public void fileChannel() throws IOException {
+  public void fileChannel(boolean useBuffer) throws IOException {
 
     File file = new File("/opt/data/channelDemo");
     //文件目录, 追加写
@@ -34,24 +34,32 @@ public class BasicChannel {
 
 //    buffer.clear();
     ByteBuffer buffer = ByteBuffer.allocate(3);
-    while (inChannel.read(buffer) != -1) {
-      //转换
-      buffer.flip();
-      if (buffer.remaining() > 0) {
-        System.out.println("开始写");
-        outChannel.write(buffer);
-        //复位
-        buffer.clear();
-      }
-    }
 
+    if (!useBuffer) {
+      inChannel.transferFrom(outChannel, 0, inChannel.size());
+    } else {
+
+      while (inChannel.read(buffer) != -1) {
+        //转换
+        buffer.flip();
+        if (buffer.remaining() > 0) {
+          System.out.println("开始写");
+          outChannel.write(buffer);
+          //复位
+          buffer.clear();
+        }
+      }
+
+    }
+    inputStream.close();
+    outputStream.close();
 
   }
 
 
   public static void main(String[] args) throws IOException {
     BasicChannel basicChannel = new BasicChannel();
-    basicChannel.fileChannel();
+    basicChannel.fileChannel(false);
   }
 
 
