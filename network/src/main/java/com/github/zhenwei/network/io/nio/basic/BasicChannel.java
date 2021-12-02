e6ac33aa3d3f36f3f68c8a0659ccf8184194f682
zhangzhenwei <zhenwei1108@163.com>
2021-12-02 22:55:42 +0800

    补充SocketChannel用例

--- network/src/main/java/com/github/zhenwei/network/io/nio/basic/BasicChannel.java
+++ network/src/main/java/com/github/zhenwei/network/io/nio/basic/BasicChannel.java
@@ -5,12 +5,19 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.channels.FileChannel.MapMode;
 import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 import java.nio.channels.WritableByteChannel;
+import java.util.ArrayList;
+import java.util.Arrays;
+import lombok.val;
 
 /**
  * {@link java.nio.channels.Channel}
@@ -65,10 +72,47 @@ public class BasicChannel {
 
   }
 
+  /**
+   * 基于 {@link java.nio.channels.NetworkChannel} 实现端口绑定 具体实现者为:
+   *
+   * @throws IOException
+   * @see sun.nio.ch.ServerSocketChannelImpl
+   * @see sun.nio.ch.SocketChannelImpl
+   */
+  public void networkChannel(int port) throws Exception {
+    ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+    SocketAddress socketAddress = new InetSocketAddress(port);
+    serverSocketChannel.bind(socketAddress);
+    System.out.println("启动端口:" + port);
+    //可以使用telnet发送数据
+    SocketChannel channel = serverSocketChannel.accept();
+    val buffers = new ArrayList<ByteBuffer>();
+    buffers.add(ByteBuffer.allocate(10));
+    buffers.add(ByteBuffer.allocate(20));
+    buffers.add(ByteBuffer.allocate(30));
+    val byteBuffers = buffers.toArray(new ByteBuffer[0]);
+    channel.read(byteBuffers);
 
-  public static void main(String[] args) throws IOException {
+    Arrays.stream(byteBuffers).forEach(System.out::println);
+
+    Arrays.stream(byteBuffers).forEach(buf -> {
+      buf.flip();
+      byte[] bytes = new byte[buf.remaining()];
+      buf.get(bytes);
+      System.out.println(new String(bytes));
+    });
+
+//    SocketChannel socketChannel = SocketChannel.open();
+//    socketAddress = new InetSocketAddress("localhost",18088);
+//    socketChannel.connect(socketAddress);
+
+  }
+
+
+  public static void main(String[] args) throws Exception {
     BasicChannel basicChannel = new BasicChannel();
-    basicChannel.fileChannel(false);
+//    basicChannel.fileChannel(false);
+    basicChannel.networkChannel(18088);
   }
 
 
