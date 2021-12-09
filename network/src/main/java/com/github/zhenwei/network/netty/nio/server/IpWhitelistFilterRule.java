0bea7ded55d8cf1ab6b40e79653810c72691b381
zhenwei1108 <zhangzhenwei@bjca.org.cn>
2021-12-09 21:54:31 +0800

    调整白名单逻辑

--- network/src/main/java/com/github/zhenwei/network/netty/nio/server/IpWhitelistFilterRule.java
+++ network/src/main/java/com/github/zhenwei/network/netty/nio/server/IpWhitelistFilterRule.java
@@ -3,29 +3,35 @@ package com.github.zhenwei.network.netty.nio.server;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.ipfilter.IpFilterRule;
 import io.netty.handler.ipfilter.IpFilterRuleType;
-
 import java.net.InetSocketAddress;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
- * @author zhenwei
- * {@link IpFilterRule}
- * {@link io.netty.handler.ipfilter.RuleBasedIpFilter#accept(ChannelHandlerContext, InetSocketAddress)}
- *
+ * @author zhenwei {@link IpFilterRule} {@link io.netty.handler.ipfilter.RuleBasedIpFilter#accept(ChannelHandlerContext,
+ * InetSocketAddress)}
+ * <p>
  * matches方法, 若返回true,则进行自定义的规则判断. ruleType()
  */
 public class IpWhitelistFilterRule implements IpFilterRule {
 
-    @Override
-    public boolean matches(InetSocketAddress remoteAddress) {
-        System.out.println("IP-Filter:" + remoteAddress.getHostName());
-        return !"127.0.0.1".equalsIgnoreCase(remoteAddress.getAddress().getHostAddress());
-    }
+  private static final ConcurrentHashMap<Integer, String> WHITE_LIST = new ConcurrentHashMap<>();
 
-    /**
-     * 只允许本机访问
-     */
-    @Override
-    public IpFilterRuleType ruleType() {
-        return IpFilterRuleType.REJECT;
-    }
+  static {
+    WHITE_LIST.put("127.0.0.1".hashCode(), "127.0.0.1");
+  }
+
+
+  @Override
+  public boolean matches(InetSocketAddress remoteAddress) {
+    System.out.println("IP-Filter:" + remoteAddress.getHostName());
+    return !WHITE_LIST.contains(remoteAddress.getHostName());
+  }
+
+  /**
+   * 只允许本机访问
+   */
+  @Override
+  public IpFilterRuleType ruleType() {
+    return IpFilterRuleType.REJECT;
+  }
 }
