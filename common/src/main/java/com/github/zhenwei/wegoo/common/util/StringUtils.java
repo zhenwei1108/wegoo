f95ca4b50edaf269de9c69dfd0bce6817332e495
zhenwei1108 <zhenwei1108@163.com>
2021-12-28 23:00:22 +0800

    补充工具类

--- common/src/main/java/com/github/zhenwei/wegoo/common/util/StringUtils.java
+++ common/src/main/java/com/github/zhenwei/wegoo/common/util/StringUtils.java
@@ -3,8 +3,25 @@ package com.github.zhenwei.wegoo.common.util;
 public class StringUtils {
 
 
+  /**
+   * @param [msg]
+   * @return boolean
+   * @author zhangzhenwei
+   * @description 判断是否为空
+   * @date 2021/12/28 23:00
+   */
   public static boolean isEmpty(String msg) {
     return msg == null || msg.length() == 0;
   }
 
+  /**
+   * @param [msg]
+   * @return boolean
+   * @author zhangzhenwei
+   * @description 补充空串的校验
+   * @date 2021/12/28 23:00
+   */
+  public static boolean isBlank(String msg) {
+    return isEmpty(msg) || msg.trim().length() == 0;
+  }
 }
