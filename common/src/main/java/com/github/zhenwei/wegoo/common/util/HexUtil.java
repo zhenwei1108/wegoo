f8448fff4b909843482e27ced1bae5a11419acf2
zhenwei <zhenwei1108@163.com>
2022-01-04 23:05:12 +0800

    补充工具类

---
+++ common/src/main/java/com/github/zhenwei/wegoo/common/util/HexUtil.java
@@ -0,0 +1,16 @@
+package com.github.zhenwei.wegoo.common.util;
+
+public class HexUtil {
+    private static final char[] hexCode = "0123456789ABCDEF".toCharArray();
+
+    public static String toString(byte[] data) {
+        StringBuilder r = new StringBuilder(data.length * 2);
+        for (byte b : data) {
+            r.append(hexCode[(b >> 4) & 0xF]);
+            r.append(hexCode[(b & 0xF)]);
+        }
+        return r.toString();
+    }
+
+
+}
