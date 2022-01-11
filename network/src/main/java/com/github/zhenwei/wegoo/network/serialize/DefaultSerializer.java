2485e00968a5c8c0ad0dbe17bbce297e6b314daa
zhenwei1108 <zhenwei1108@163.com>
2022-01-11 23:14:35 +0800

    参考redis 调整序列化方式

--- network/src/main/java/com/github/zhenwei/wegoo/network/serialize/DefaultSerializer.java
+++ network/src/main/java/com/github/zhenwei/wegoo/network/serialize/DefaultSerializer.java
@@ -1,21 +1,28 @@
 package com.github.zhenwei.wegoo.network.serialize;
 
 
+import com.google.protobuf.MessageLite;
+import lombok.AllArgsConstructor;
+
 /**
  * @description: 默认序列化实现
+ *  需要引入 Protobuf-java 依赖
  * @author: zhangzhenwei
  * @date: 2022/1/8 22:02
  */
-public class DefaultSerializer implements NettySerializer {
+@AllArgsConstructor
+public class DefaultSerializer<T extends MessageLite> implements NettySerializer<T> {
 
+  private final MessageLite prototype;
 
   @Override
-  public byte[] serialize(Object o) throws Exception {
+  public byte[] serialize(T o) throws Exception {
+
     return new byte[0];
   }
 
   @Override
-  public Object deserialize(byte[] bytes) throws Exception {
-    return null;
+  public T deserialize(byte[] bytes) throws Exception {
+    return (T) prototype.getParserForType().parseFrom(bytes);
   }
 }
