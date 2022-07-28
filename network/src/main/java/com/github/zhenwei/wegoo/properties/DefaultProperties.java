7f6f755e84f3f3bc90ba9ef4f51621ad93c07f13
zhenwei1108 <zhenwei1108@163.com>
2022-07-28 22:59:15 +0800

    完善工具类

---
+++ network/src/main/java/com/github/zhenwei/wegoo/properties/DefaultProperties.java
@@ -0,0 +1,11 @@
+package com.github.zhenwei.wegoo.properties;
+
+import lombok.Data;
+
+@Data
+public class DefaultProperties {
+
+    private byte[] iv = new byte[16];
+
+
+}
--- network/src/main/java/com/github/zhenwei/wegoo/security/CryptoUtil.java
+++ network/src/main/java/com/github/zhenwei/wegoo/security/CryptoUtil.java
@@ -1,10 +1,22 @@
 package com.github.zhenwei.wegoo.security;
 
+import com.github.zhenwei.core.enums.CipherAlgEnum;
+import com.github.zhenwei.core.exception.WeGooCipherException;
+import com.github.zhenwei.sdk.builder.CipherBuilder;
+import com.github.zhenwei.wegoo.properties.DefaultProperties;
+
+import java.security.Key;
+
 public class CryptoUtil {
 
-    public static void encrypt(byte[] data){
-        cipherbu
-
+    public static byte[] sm4CbcEncryptData(byte[] data, Key key, DefaultProperties properties) throws WeGooCipherException {
+        return CipherBuilder.cipher(CipherAlgEnum.SM4_CBC_PKCS7Padding, key, data, properties.getIv(), true);
     }
+
+
+    public static byte[] sm4CbcDecryptData(byte[] data, Key key, DefaultProperties properties) throws WeGooCipherException {
+        return CipherBuilder.cipher(CipherAlgEnum.SM4_CBC_PKCS7Padding, key, data, properties.getIv(), false);
+    }
+
 
 }
