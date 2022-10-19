d7707b9cf580ee60ae547afc3666480592b6b6c3
zhenwei1108 <zhenwei1108@163.com>
2022-10-19 17:18:28 +0800

    add desc

--- network/src/main/java/com/github/zhenwei/wegoo/io/IoProvider.java
+++ network/src/main/java/com/github/zhenwei/wegoo/io/IoProvider.java
@@ -1,5 +1,12 @@
 package com.github.zhenwei.wegoo.io;
 
+/**
+ * @author: zhangzhenwei
+ * @description: IoProvider
+ *  顶级IO处理类
+ * @date: 2022/10/19  17:17
+ * @since: 1.0
+ */
 public interface IoProvider {
 
     void write(byte[] data);
--- network/src/main/java/com/github/zhenwei/wegoo/io/nio/INioProvider.java
+++ network/src/main/java/com/github/zhenwei/wegoo/io/nio/INioProvider.java
@@ -1,6 +1,11 @@
 package com.github.zhenwei.wegoo.io.nio;
 
 import com.github.zhenwei.wegoo.io.IoProvider;
-
-public interface INioProvider extends IoProvider {
-}
+/**
+ * @author: zhangzhenwei
+ * @description: INioProvider
+ *  NIO 处理类
+ * @date: 2022/10/19  17:18
+ * @since: 1.0
+ */
+public interface INioProvider extends IoProvider { }
